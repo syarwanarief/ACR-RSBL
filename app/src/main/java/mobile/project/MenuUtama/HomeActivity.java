4cd834360389a990bf4e927f4ef57be3a8948a94
syarwanarief <ariefsyarwan@gmail.com>
2018-10-11 01:23:58 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- app/src/main/java/mobile/project/MenuUtama/HomeActivity.java
+++ app/src/main/java/mobile/project/MenuUtama/HomeActivity.java
@@ -2,9 +2,14 @@
 package mobile.project.MenuUtama;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import mobile.project.rsbl.AbdulMoeloek;
 import mobile.project.rsbl.R;
@@ -16,10 +21,19 @@ import mobile.project.rsbl.RestuBunda;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private SliderPagerAdapter mAdapter;
+    private SliderIndicator mIndicator;
+
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        sliderView = (SliderView) findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) findViewById(R.id.pagesContainer);
+        setupSlider();
     }
 
     public void RSabdulMoeloek(View view) {
@@ -50,5 +64,20 @@ public class HomeActivity extends AppCompatActivity {
     public void RSIArestubunda(View view) {
         Intent intent = new Intent(HomeActivity.this, RestuBunda.class);
         startActivity(intent);
+    }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-1.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-2.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-3.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-4.jpg"));
+
+        mAdapter = new SliderPagerAdapter(getSupportFragmentManager(), fragments);
+        sliderView.setAdapter(mAdapter);
+        mIndicator = new SliderIndicator(this, mLinearLayout, sliderView, R.drawable.indicator_circle);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
     }
 }
